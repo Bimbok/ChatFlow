40ebb6ea2bb04fbcdd92e01db3770719d3a04016
Bimbok <bimbokmkj@gmail.com>
2025-12-26 13:12:12 +0530

    i-dont-know

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const dotenv = require("dotenv");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const path = require("path");
-
+import cors from "cors";
 const { app, server } = require("./src/lib/socket");
 const connectDB = require("./src/lib/db");
 const authRoutes = require("./src/routes/auth.route");
@@ -20,7 +20,9 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    // origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: "https://one-to-one-chat-app-two.vercel.app",
+
     credentials: true,
   }),
 );
@@ -38,4 +40,3 @@ connectDB();
 server.listen(PORT, () => {
   console.log("server is running on PORT:" + PORT);
 });
-
--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -8,8 +8,8 @@ const generateToken = (userId, res) => {
   res.cookie("jwt", token, {
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days in MS
     httpOnly: true, // prevent XSS attacks cross-site scripting attacks
-    sameSite: process.env.NODE_ENV !== "development" ? "None" : "strict",
-    secure: process.env.NODE_ENV !== "development",
+    sameSite: "None",
+    secure: true,
   });
 
   return token;
